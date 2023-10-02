731a9b2ec0e0aa39441e957798d20aac833996ed
2toesxtra <devkelly9622@gmail.com>
2023-10-02 04:07:48 -0400

    picture change

--- src/partials/Hero.tsx
+++ src/partials/Hero.tsx
@@ -3,16 +3,16 @@ import { React } from 'react';
 const Hero = () => (
   <div className="grid grid-cols-1 justify-items-center text-center">
     <img
-      src="https://res.cloudinary.com/dq1ktd5ge/image/upload/v1669765799/portfolio/headshot_1_n3osrc.png"
+      src="https://i.ibb.co/T0r9Q59/Untitled-1.png"
       className="mb-4 w-64 saturate-50"
     />
     <h1 className="mb-4 text-4xl font-bold text-black">
-      Hello, World - I'm Devin Kelly
+      About Devin Kelly
     </h1>
     <h2 className="text-xl font-medium text-black">
-      Full-Stack Web Developer & Content Writer
+      Full-Stack Web Developer
     </h2>
-    <h3 className="mb-4 text-lg text-black">ReactJS | NextJS | Django</h3>
+    <h3 className="mb-4 text-lg text-black">ReactJS | ExpressJS | MongoDB</h3>
     <a
       className="rounded border border-black bg-transparent py-2 px-4 font-semibold text-black duration-500 hover:border-transparent hover:bg-slate-900 hover:text-white"
       href="/about"
